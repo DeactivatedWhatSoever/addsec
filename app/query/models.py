5aeda494b88de846e2cef3e9bf1609c42f51d069
RobinRheem <roborheems@gmail.com>
2019-01-12 23:31:10 +0900

    Finish modeling queries and query_results

--- app/query/models.py
+++ app/query/models.py
@@ -5,48 +5,62 @@ class Query(db.Model):
     __tablename__ = 'queries'
 
     id = db.Column(db.Integer, primary_key=True)
-    query_score_id = db.Column(db.Integer)
+    query_result_id = db.Column(db.Integer)
+    symbol = db.Column(db.String(255), nullable=False)
     parent_address = db.Column(db.String(255), nullable=False)
     child_address = db.Column(db.String(255), nullable=False)
-    query_count = db.Column(db.Integer, nullable=False)
-    final_score = db.Column(db.Float(), nullable=False)
+    current_score = db.Column(db.Float(), nullable=False)
     depth = db.Column(db.Integer, nullable=False)
+    fraud = db.Column(db.Integer, nullable=False)
+    curious = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime,  default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime,  default=db.func.current_timestamp(), 
            onupdate=db.func.current_timestamp())
 
-    def __init__(self, parent_address, child_address, depth):
+    def __init__(self, symbol, parent_address, child_address, current_score, depth, fraud, curious):
+        self.symbol = symbol
         self.parent_address = parent_address
         self.child_address = child_address
+        self.current_score = current_score
         self.depth = depth
+        self.fraud = fraud
+        self.curious = curious
 
     def __repr__(self):
-        return '<Query %r, %r, %r, %r, %r, %r>' % (
+        return '<Query %r, %r, %r, %r, %r, %r, %r, %r, %r>' % (
                 self.id,
+                self.symbol,
                 self.parent_addres,
                 self.child_address,
                 self.query_count,
                 self.final_score,
-                self.depth
+                self.depth,
+                self.fraud,
+                self.curious
         )
 
-class QueryScore(db.Model):
-    __tablename__ = 'query_scores'
+
+class QueryResult(db.Model):
+    __tablename__ = 'query_results'
 
     id = db.Column(db.Integer, primary_key=True)
-    base_score = db.Column(db.Float(), nullable=False)
-    applied_score = db.Column(db.Float(), nullable=False)
+    avg_score = db.Column(db.Float(), nullable=False)
+    total_fraud = db.Column(db.Float(), nullable=False)
+    total_curious = db.Column(db.Float(), nullable=False)
     created_at = db.Column(db.DateTime,  default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime,  default=db.func.current_timestamp(), 
            onupdate=db.func.current_timestamp())
 
-    def __init__(self, base_score, applied_score):
-        self.base_score = base_score
-        self.applied_score = applied_score
+    def __init__(self, avg_score, total_fraud, total_curious):
+        self.avg_score = avg_score
+        self.total_fraud = total_fraud
+        self.total_curious = total_curious
 
     def __repr__(self):
-        return '<QueryScore %r, %r, %r>' % (
+        return '<QueryResult %r, %r, %r>' % (
                 self.id,
-                self.base_score,
-                self.applied_score
+                self.avg_score,
+                self.total_fraud,
+                self.total_curious
         )
+
